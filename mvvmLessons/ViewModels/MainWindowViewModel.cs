66ef612fd469af9ee84f81411b4f2cd0c954b791
pinkxpink00 <kyperu07@gmail.com>
2023-11-28 22:27:06 +0200

    Binding Text

--- mvvmLessons/ViewModels/MainWindowViewModel.cs
+++ mvvmLessons/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mvvmLessons.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace mvvmLessons.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _Title = "Анализ статистики CV19";
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if (Equals(value, _Title)) return;
+                _Title = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
     }
 }
