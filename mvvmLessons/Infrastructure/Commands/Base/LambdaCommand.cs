7789db9e87cbd9ecd250e52e842821aeba9a816b
pinkxpink00 <kyperu07@gmail.com>
2023-12-03 23:01:28 +0200

    create LambdaCommand

---
+++ mvvmLessons/Infrastructure/Commands/Base/LambdaCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvmLessons.Infrastructure.Commands.Base
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object,bool> CanExecute =null)
+        {
+            execute = Execute;
+            canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
