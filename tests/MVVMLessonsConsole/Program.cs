241a95bbf180c5b9dc1d98d0a434acda88330b8d
pinkxpink00 <kyperu07@gmail.com>
2023-12-07 22:18:16 +0200

    try to going in another coin api

--- tests/MVVMLessonsConsole/Program.cs
+++ tests/MVVMLessonsConsole/Program.cs
@@ -5,74 +5,112 @@ namespace MVVMLessonsConsole
 {
     class Program
     {
-        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-        private static async Task<Stream> GetDataStream()
+         static async Task Main()
+    {
+        string url = "https://api.coingecko.com/api/v3/ping";
+
+        using (HttpClient client = new HttpClient())
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync(data_url,HttpCompletionOption.ResponseHeadersRead);
-            return await response.Content.ReadAsStreamAsync();
-        }
-
-        private static  IEnumerable<string> GetDataLines()
-        {
-             using var data_stream =  GetDataStream().Result;
-             using var data_reader = new StreamReader(data_stream);
-
-            while (!data_reader.EndOfStream)
+            try
             {
-                var line =  data_reader.ReadLine();
-                if (string.IsNullOrEmpty(line)) continue;
-                yield return line.Replace("Korea,","Korea -"); 
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Запрос успешен!");
+                    Console.WriteLine(responseBody);
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка запроса. Код состояния: {response.StatusCode}");
+                    Console.WriteLine(await response.Content.ReadAsStringAsync());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Возникла ошибка: {ex.Message}");
             }
         }
+    }
 
-        private static DateTime[] GetDates() => GetDataLines()
-            .First()
-            .Split(',')
-            .Skip(4)
-            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
-            .ToArray();
+        //private const string data_url = @"https://microsoft.com/";
 
-        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
-        {
-            var lines = GetDataLines()
-                .Skip(1)
-                .Select(line => line.Split(','));
+        ////private static async Task<Stream> GetDataStream()
+        ////{
+        ////    var client = new HttpClient();
+        ////    var response = await client.GetAsync(data_url,HttpCompletionOption.ResponseHeadersRead);
+        ////    return await response.Content.ReadAsStreamAsync();
+        ////}
 
-            foreach (var row in lines)
-            {
-                var province = row[0].Trim();
-                var country_name = row[1].Trim(' ', '"');
-                var counts = row.Skip(4).Select(int.Parse).ToArray();
+        //static  void Main(string[] args)
+        //{
+        //    using var client = new HttpClient();
+        //    var result =  client.GetStringAsync(data_url);
 
-                yield return (province, country_name, counts);
-            }
-        }
+        //    var content = response.Content.ReadAsStringAsync();
+        //}
 
-        static void Main(string[] args)
-        {
-            //var client = new HttpClient();
+        //private static  IEnumerable<string> GetDataLines()
+        //{
+        //     using var data_stream =  GetDataStream().Result;
+        //     using var data_reader = new StreamReader(data_stream);
 
-            //var response = client.GetAsync(data_url).Result;
+        //    while (!data_reader.EndOfStream)
+        //    {
+        //        var line =  data_reader.ReadLine();
+        //        if (string.IsNullOrEmpty(line)) continue;
+        //        yield return line.Replace("Korea,","Korea -"); 
+        //    }
+        //}
 
-            //var csv_str = response.Content.ReadAsStringAsync().Result;
+        //private static DateTime[] GetDates() => GetDataLines()
+        //    .First()
+        //    .Split(',')
+        //    .Skip(4)
+        //    .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+        //    .ToArray();
 
-            //foreach(var data_line in GetDataLines())
-            //    Console.WriteLine(data_line);
+        //private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
+        //{
+        //    var lines = GetDataLines()
+        //        .Skip(1)
+        //        .Select(line => line.Split(','));
 
-            //var dates = GetDates();
+        //    foreach (var row in lines)
+        //    {
+        //        var province = row[0].Trim();
+        //        var country_name = row[1].Trim(' ', '"');
+        //        var counts = row.Skip(4).Select(int.Parse).ToArray();
 
-            //Console.WriteLine(string.Join("\r\n",dates));
+        //        yield return (province, country_name, counts);
+        //    }
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    //var client = new HttpClient();
+
+        //    //var response = client.GetAsync(data_url).Result;
+
+        //    //var csv_str = response.Content.ReadAsStringAsync().Result;
+
+        //    //foreach(var data_line in GetDataLines())
+        //    //    Console.WriteLine(data_line);
+
+        //    //var dates = GetDates();
+
+        //    //Console.WriteLine(string.Join("\r\n",dates));
 
 
-            var ukraine_data = GetData()
-                .First(v => v.Country.Equals("Ukraine",StringComparison.OrdinalIgnoreCase));
+        //    var ukraine_data = GetData()
+        //        .First(v => v.Country.Equals("Ukraine"));
 
-            Console.WriteLine(string.Join("\r\n",GetDates().Zip(ukraine_data.Counts,(date,count)=>$"{date}-{count}")));
+        //    Console.WriteLine(string.Join("\r\n",GetDates().Zip(ukraine_data.Counts,(date,count)=>$"{date}-{count}")));
 
-            Console.ReadLine();
-        }
+        //    Console.ReadLine();
+        //}
 
     }
 }
