e83e3639f57c7ba282ab0805ba3da0ab1d15b6c5
pinkxpink00 <kyperu07@gmail.com>
2023-12-22 00:23:31 +0200

    Add new folder

---
+++ mvvmLessons/Models/Decanat/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvmLessons.Models.Decanat
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public double Rating { get; set; }
+    }
+}
